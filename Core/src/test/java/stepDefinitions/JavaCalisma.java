0718e41d4d4b43392429038628cd3d238954d25c
omeryttnc <omeryttnc@gmail.com>
2024-02-09 00:33:11 +0000

    week 2 us 50 download assertion css assertion

--- Core/src/test/java/stepDefinitions/JavaCalisma.java
+++ Core/src/test/java/stepDefinitions/JavaCalisma.java
@@ -1,6 +1,8 @@
 package stepDefinitions;
 
 import junit.framework.TestCase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 
@@ -22,5 +24,11 @@ public class JavaCalisma extends TestCase {
 
     public String getFlagPath(String flag) {
         return System.getProperty("user.dir") + "\\src\\test\\resources\\userLogo\\"+flag+".png";
+    }
+
+    public void testName() {
+        WebElement webElement = null;
+        Select select = new Select(webElement );
+        select.selectByIndex(2);
     }
 }
